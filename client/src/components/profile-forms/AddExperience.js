5d3e8d9cd337bb25ef203ae4ca005ff38f96cf1e
Andela Culjak <andela212@gmail.com>
2019-08-06 02:21:02 +0200

    All profiles page done

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -111,9 +111,9 @@ const AddExperience = ({ addExperience, history }) => {
           />
         </div>
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
+        <Link className="btn btn-light my-1" to="dashboard">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
@@ -126,4 +126,4 @@ AddExperience.propTypes = {
 export default connect(
   null,
   { addExperience }
-)(AddExperience);
+)(withRouter(AddExperience));
